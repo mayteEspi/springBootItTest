50b046a49900d47921e18c0be1f7f3cfa0014631
mayteEspi <maytehermida@gmail.com>
2018-01-30 16:11:39 +0100

    convert to spring boot project

---
+++ springboot-integrationtest/src/main/java/com/example/demo/SpringbootIntegrationtestApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIntegrationtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIntegrationtestApplication.class, args);
+	}
+}
