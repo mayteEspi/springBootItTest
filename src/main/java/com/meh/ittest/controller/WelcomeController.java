13e8809f59d241eb81b74ff3c585ad0ef7013797
mayteEspi <maytehermida@gmail.com>
2018-01-30 16:31:11 +0100

    add simple controller

---
+++ src/main/java/com/meh/ittest/controller/WelcomeController.java
@@ -0,0 +1,30 @@
+package com.meh.ittest.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.meh.ittest.model.UserModel;
+
+@RestController
+public class WelcomeController {
+
+	private static Log log = LogFactory.getLog(WelcomeController.class);
+	
+	@GetMapping("/welcome")
+	public ResponseEntity<UserModel> welcome() {
+		ResponseEntity<UserModel> response = null;
+		try{
+			response = new ResponseEntity<>(new UserModel("Mayte"), HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e);
+		}
+		return response;
+	}
+}
---
+++ src/main/java/com/meh/ittest/model/UserModel.java
@@ -0,0 +1,24 @@
+package com.meh.ittest.model;
+
+public class UserModel {
+	
+	private String userName;
+
+	
+	public UserModel(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	
+
+	
+
+}
